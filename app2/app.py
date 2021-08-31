56391eeea01249c474b59d240ab44200566d082c
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-08-31 08:20:00 +0200

    Add progress bar logic

--- app2/app.py
+++ app2/app.py
@@ -9,22 +9,49 @@ import diskcache
 cache = diskcache.Cache("./cache")
 long_callback_manager = DiskcacheLongCallbackManager(cache)
 
-app = dash.Dash(__name__)
+app = dash.Dash(
+    __name__,
+    long_callback_manager=long_callback_manager,
+)
 
 app.layout = html.Div(
     [
-        html.Div([html.P(id="paragraph_id", children=["Button not clicked"])]),
+        html.Div(
+            [
+                html.P(id="paragraph_id", children=["Button not clicked"]),
+                html.Progress(id="progress_bar"),
+            ]
+        ),
         html.Button(id="button_id", children="Run Job!"),
+        html.Button(id="cancel_button_id", children="Cancel Running Job!"),
     ]
 )
 
 @app.long_callback(
     output=Output("paragraph_id", "children"),
     inputs=Input("button_id", "n_clicks"),
-    manager=long_callback_manager,
+    running=[
+        (Output("button_id", "disabled"), True, False),
+        (Output("cancel_button_id", "disabled"), False, True),
+        (
+            Output("paragraph_id", "style"),
+            {"visibility": "hidden"},
+            {"visibility": "visible"},
+        ),
+        (
+            Output("progress_bar", "style"),
+            {"visibility": "visible"},
+            {"visibility": "hidden"},
+        ),
+    ],
+    cancel=[Input("cancel_button_id", "n_clicks")],
+    progress=[Output("progress_bar", "value"), Output("progress_bar", "max")],
 )
-def callback(n_clicks):
-    time.sleep(2.0)
+def callback(set_progress, n_clicks):
+    total = 10
+    for i in range(total):
+        time.sleep(0.5)
+        set_progress((str(i + 1), str(total)))
     return [f"Clicked {n_clicks} times"]
 
 
