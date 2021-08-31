00b0b6094d79077b624f97dcd582d9ba66784120
José Luiz Ferreira <jlfsjunior@gmail.com>
2021-08-31 08:03:15 +0200

    Add example app

---
+++ app2/app.py
@@ -0,0 +1,32 @@
+import time
+import dash
+from dash import html
+from dash.long_callback import DiskcacheLongCallbackManager
+from dash.dependencies import Input, Output
+
+## Diskcache
+import diskcache
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        html.Div([html.P(id="paragraph_id", children=["Button not clicked"])]),
+        html.Button(id="button_id", children="Run Job!"),
+    ]
+)
+
+@app.long_callback(
+    output=Output("paragraph_id", "children"),
+    inputs=Input("button_id", "n_clicks"),
+    manager=long_callback_manager,
+)
+def callback(n_clicks):
+    time.sleep(2.0)
+    return [f"Clicked {n_clicks} times"]
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
